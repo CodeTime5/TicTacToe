cfb742c76b13e0dcaf30b7522e6aa0704114eb74
Ed Potter <ebpotter@perse.co.uk>
2020-04-20 10:22:05 +0100

    MVP

--- Game.py
+++ Game.py
@@ -1,5 +1,4 @@
 class Game:
-
     EMPTY = " "
     DIM = 3
     P1 = "o"
@@ -10,7 +9,13 @@ class Game:
         self._player = Game.P1
 
     def __repr__(self):
-        return ""
+        result = "  " + " ".join(str(i+1) for i in range(Game.DIM))
+        for row in range(Game.DIM):
+            result += f"\n{row + 1} {self._board[row][0]}|{self._board[row][1]}|{self._board[row][2]}"
+            if row + 1 != Game.DIM:
+                result += "\n  -----"
+        result += f"\n\n{self._player} turn to play"
+        return result
 
     def play(self, row, col):
         row += -1
@@ -20,7 +25,22 @@ class Game:
     
     @property
     def winner(self):
-        pass
+        for p in [Game.P1, Game.P2]:
+            for row in range(3):
+                if all(self._board[row][col] is p for col in range(3)):
+                    return p
+            for col in range(3):
+                if all(self._board[row][col] is p for row in range(3)):
+                    return p
+            ## Diagonals
+            if all(self._board[i][i] is p for i in range(3)):
+                return p
+            if all(self._board[i][2 - i] is p for i in range(3)):
+                return p
+        
+        ## No winner
+        return None
+
 
 if __name__ == "__main__":
     pass
